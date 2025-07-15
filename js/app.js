dafd97d94ce96a6774018af67df301526083fce3
JulienEG <elghadghadi.julien@gmail.com>
2025-07-15 17:48:27 +0200

    Simple sequence execution working

--- js/app.js
+++ js/app.js
@@ -1,31 +1,52 @@
 const app = new Vue({
   el: "#app",
   data: {
-    notes: [" ", "A", "A#", "B", "B#", "C", "C#", "D", "D#" ,"E" ,"E#" ,"F" ,"F#" ,"G", "G#"],
+    notes: [
+      " ",
+      "A",
+      "A#",
+      "B",
+      "B#",
+      "C",
+      "C#",
+      "D",
+      "D#",
+      "E",
+      "E#",
+      "F",
+      "F#",
+      "G",
+      "G#",
+    ],
     currentNoteIndex: 0,
     pitches: [" ", "1", "2", "3", "4", "5", "6", "7", "8"],
     currentPitchIndex: 0,
     effects: [" ", "a", "b", "c", "d", "e"],
     currentEffectIndex: 0,
-    pulse1Notes: [],
+    pulse1Notes: ["C4", "E4", "G4", "B4", null, "C5", null, "G4"],
     pulse2Notes: [],
     waveNotes: [],
     noiseNotes: [],
     pu1Synth: null,
     pu2Synth: null,
+    bpm: 128,
   },
   mounted() {
     // Synths initialisation
     this.pu1Synth = new Tone.Synth({
       oscillator: {
         type: "pulse",
-        width: 0.125 // 0.125, 0.25, 0.50, 0.75
-      }
+        width: 0.125, // 0.125, 0.25, 0.50, 0.75
+      },
     }).toDestination();
-
-
   },
+
   computed: {},
+  watch: {
+    bpm(newBpm) {
+      Tone.Transport.bpm.value = newBpm;
+    },
+  },
 
   methods: {
     onScrollCell(event, indexName, arrayName) {
@@ -46,19 +67,47 @@ const app = new Vue({
       this[indexName] = 0;
     },
 
-    async playNote() {
-      if (Tone.context !== "running") {
+    onScrollBpm(event) {
+      let step = 1;
+      if (event.shiftKey) step = 10;
+      if (event.ctrlKey) step = 5;
+
+      const delta = Math.sign(event.deltaY);
+      if (delta < 0 && this.bpm < 255) {
+        this.bpm = Math.min(255, this.bpm + step);
+      } else if (delta > 0 && this.bpm > 1) {
+        this.bpm = Math.max(1, this.bpm - step);
+      }
+    },
+
+    async playSequence() {
+      if (Tone.context.state !== "running") {
         await Tone.start;
       }
-      this.pu1Synth.triggerAttackRelease("C4", "16n");
+      const sequence = new Tone.Sequence(
+        (time, note) => {
+          this.pu1Synth.triggerAttackRelease(note, "8n", time);
+        },
+        this.pulse1Notes,
+        "8n"
+      );
+
+      Tone.Transport.bpm.value = this.bpm;
+      sequence.start(0); // Lance la séquence
+      Tone.Transport.start(); // Démarre le transport (la "clock" globale)
+      // Tone.Transport.stop();
+    },
+
+    stopSequence() {
+      Tone.Transport.stop();
     },
 
     //// Synths Methods ////
-    
+
     setPulseWidth(synth, width) {
       synth.oscillator.width = width;
     },
-  }
+  },
 });
 
 
