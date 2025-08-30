5c1e51f3fd109e334b230edc73ff9f0f1b62cbb2
JulienEG <elghadghadi.julien@gmail.com>
2025-08-30 15:15:24 +0200

    Can scroll by octave with ctrl pressed

--- js/app.js
+++ js/app.js
@@ -102,14 +102,32 @@ const app = new Vue({
     ],
     notes: [
       null,
-      "A1", "A#1", "B1", "B#1", "C1", "C#1", "D1", "D#1", "E1", "E#1", "F1", "F#1", "G1", "G#1", 
-      "A2", "A#2", "B2", "B#2", "C2", "C#2", "D2", "D#2", "E2", "E#2", "F2","F#2","G2","G#2",
-      "A3", "A#3", "B3", "B#3", "C3", "C#3", "D3", "D#3", "E3", "E#3", "F3","F#3","G3","G#3",
-      "A4", "A#4", "B4", "B#4", "C4", "C#4", "D4", "D#4", "E4", "E#4", "F4","F#4","G4","G#4",
-      "A5", "A#5", "B5", "B#5", "C5", "C#5", "D5", "D#5", "E5", "E#5","F5","F#5","G5","G#5",
-      "A6", "A#6", "B6", "B#6", "C6", "C#6", "D6", "D#6", "E6", "E#6","F6","F#6","G6","G#6",
-      "A7", "A#7", "B7", "B#7", "C7", "C#7", "D7", "D#7", "E7", "E#7","F7","F#7","G7","G#7",
-      "A8", "A#8", "B8", "B#8", "C8", "C#8", "D8", "D#8", "E8", "E#8","F8","F#8","G8","G#8",
+      // Octave 0
+      "C0", "C#0", "D0", "D#0", "E0", "F0", "F#0", "G0", "G#0", "A0", "A#0", "B0",
+
+      // Octave 1
+      "C1", "C#1", "D1", "D#1", "E1", "F1", "F#1", "G1", "G#1", "A1", "A#1", "B1",
+
+      // Octave 2
+      "C2", "C#2", "D2", "D#2", "E2", "F2", "F#2", "G2", "G#2", "A2", "A#2", "B2",
+
+      // Octave 3
+      "C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3",
+
+      // Octave 4
+      "C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4", "A#4", "B4",
+
+      // Octave 5
+      "C5", "C#5", "D5", "D#5", "E5", "F5", "F#5", "G5", "G#5", "A5", "A#5", "B5",
+
+      // Octave 6
+      "C6", "C#6", "D6", "D#6", "E6", "F6", "F#6", "G6", "G#6", "A6", "A#6", "B6",
+
+      // Octave 7
+      "C7", "C#7", "D7", "D#7", "E7", "F7", "F#7", "G7", "G#7", "A7", "A#7", "B7",
+
+      // Octave 8
+      "C8"
     ],
     durations: [null, "1n", "2n", "4n", "8n", "16n"],
     velocities: [null, "pp", "p", "mp", "mf", "f", "ff"],
@@ -177,12 +195,20 @@ const app = new Vue({
       }
     },
 
-    onScrollCell(event, synthKey, indexKey, rowIndex, listName) {
-      const dir = event.deltaY > 0 ? -1 : 1;
+    onScrollCell(event, section, property, rowIndex, listName) {
+      event.preventDefault();
+    
+      const step = event.ctrlKey ? 12 : 1;
+      const delta = event.deltaY > 0 ? -step : step;
+    
+      const currentIndex = this.tableData[rowIndex][section][property];
       const list = this[listName];
-      const currentIndex = this.tableData[rowIndex][synthKey]?.[indexKey] ?? 0;
-      const newIndex = (currentIndex + dir + list.length) % list.length;
-      this.$set(this.tableData[rowIndex][synthKey], indexKey, newIndex);
+    
+      // Calcul du nouvel index avec wrap-around
+      let newIndex = (currentIndex + delta) % list.length;
+      if (newIndex < 0) newIndex += list.length; // éviter les indices négatifs
+    
+      this.tableData[rowIndex][section][property] = newIndex;
     },
     
     onClickCell(rowIndex, synthKey, indexKey) {
